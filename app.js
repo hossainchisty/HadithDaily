985c35363e32f3d735efd532ac1a872eb308acee
hossainchisty <hossain.chisty11@gmail.com>
2023-06-13 12:49:18 +0600

    refactor: change cron job runer

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cron = require('node-cron');
-const sendEmail = require('./mailer');
 const cookieParser = require('cookie-parser');
-const User = require('./schema/userModels');
-const generateRandomHadith = require('./services/getRandomHadith');
 
 
 const app = express();
@@ -40,34 +36,6 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
-});
-
-
-
-
-// Schedule the task to run at 7am daily
-
-// - 0 7 * * *
-cron.schedule('* * * * *', async () => {
-  try {
-    // Retrieve all user email addresses from the user database
-    const users = await User.find({}, 'email');
-
-    users.forEach(async (user) => {
-      const userEmailAddress = user.email;
-
-      const hadith = await generateRandomHadith();
-      const subject = 'Hadith Daily';
-      const text = `${hadith.quote}\n(Reference: ${hadith.reference})`;
-
-      sendEmail(userEmailAddress, subject, text);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}, {
-  scheduled: true,
-  timezone: "Asia/Dhaka"
 });
 
 // Set the routes
--- routers/index.js
+++ routers/index.js
@@ -5,6 +5,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const Hadith = require('../schema/hadithModels');
 const User = require('../schema/userModels');
+const cron = require('node-cron');
+const sendEmail = require('../services/mailer');
 const generateRandomHadith = require('../services/getRandomHadith');
 
 // Register route
@@ -72,6 +74,32 @@ router.post('/login', async (req, res) => {
         res.status(500).json({ message: 'Server Error' });
     }
 });
+
+
+// Schedule the task to run at 7am daily
+// * * * * *  0 7 * * *
+cron.schedule('* * * * *', async () => {
+    try {
+        // Retrieve all user email addresses from the user database
+        const users = await User.find({}, 'email');
+
+        for (const user of users) {
+            const userEmailAddress = user.email;
+
+            const hadith = await generateRandomHadith();
+            const subject = 'Hadith Daily';
+            const text = `${hadith.quote}\n(Reference: ${hadith.reference})`;
+
+            await sendEmail(userEmailAddress, subject, text);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}, {
+    scheduled: true,
+    timezone: 'Asia/Dhaka'
+});
+
 
 // Protected route - render the dashboard
 router.get('/dashboard', async (req, res) => {
